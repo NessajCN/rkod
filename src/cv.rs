4fca28860b79badc7075d3d73f28385dddb08630
Nessaj <ksnessaj@hotmail.com>
2024-07-01 15:51:03 +0800

    bump dependencies

--- src/cv.rs
+++ src/cv.rs
@@ -1,10 +1,10 @@
 extern crate ffmpeg_next as ffmpeg;
 
 use std::io::{self, Result};
-use std::path::Path;
+// use std::path::Path;
 
-use ffmpeg::format::{input, stream, Pixel};
-use ffmpeg::media::Type;
+use ffmpeg::format::{stream, Pixel};
+// use ffmpeg::media::Type;
 use ffmpeg::software::scaling::{context::Context, flag::Flags};
 use ffmpeg::util::frame::video::Video;
 use ffmpeg::{decoder, frame, picture, Packet};
@@ -79,51 +79,50 @@ impl FrameExtractor {
     }
 }
 
-/// frame_size: \[width, height\]
-fn extract_frame<P: AsRef<Path> + ?Sized>(input_path: &P, frame_size: [u32; 2]) -> Result<()> {
-    ffmpeg::init()?;
-    if let Ok(mut ictx) = input(input_path) {
-        let input = ictx
-            .streams()
-            .best(Type::Video)
-            .ok_or(ffmpeg::Error::StreamNotFound)?;
-        let video_stream_index = input.index();
+// fn extract_frame<P: AsRef<Path> + ?Sized>(input_path: &P, frame_size: [u32; 2]) -> Result<()> {
+//     ffmpeg::init()?;
+//     if let Ok(mut ictx) = input(input_path) {
+//         let input = ictx
+//             .streams()
+//             .best(Type::Video)
+//             .ok_or(ffmpeg::Error::StreamNotFound)?;
+//         let video_stream_index = input.index();
 
-        let context_decoder = ffmpeg::codec::context::Context::from_parameters(input.parameters())?;
-        let mut decoder = context_decoder.decoder().video()?;
+//         let context_decoder = ffmpeg::codec::context::Context::from_parameters(input.parameters())?;
+//         let mut decoder = context_decoder.decoder().video()?;
 
-        let mut scaler = Context::get(
-            decoder.format(),
-            decoder.width(),
-            decoder.height(),
-            Pixel::RGB24,
-            frame_size[0],
-            frame_size[1],
-            Flags::BILINEAR,
-        )?;
+//         let mut scaler = Context::get(
+//             decoder.format(),
+//             decoder.width(),
+//             decoder.height(),
+//             Pixel::RGB24,
+//             frame_size[0],
+//             frame_size[1],
+//             Flags::BILINEAR,
+//         )?;
 
-        let mut frame_index = 0;
+//         let mut frame_index = 0;
 
-        let mut receive_and_process_decoded_frames =
-            |decoder: &mut ffmpeg::decoder::Video| -> Result<()> {
-                let mut decoded = Video::empty();
-                while decoder.receive_frame(&mut decoded).is_ok() {
-                    let mut rgb_frame = Video::empty();
-                    scaler.run(&decoded, &mut rgb_frame)?;
-                    // save_file(&rgb_frame, frame_index).unwrap();
-                    frame_index += 1;
-                }
-                Ok(())
-            };
+//         let mut receive_and_process_decoded_frames =
+//             |decoder: &mut ffmpeg::decoder::Video| -> Result<()> {
+//                 let mut decoded = Video::empty();
+//                 while decoder.receive_frame(&mut decoded).is_ok() {
+//                     let mut rgb_frame = Video::empty();
+//                     scaler.run(&decoded, &mut rgb_frame)?;
+//                     // save_file(&rgb_frame, frame_index).unwrap();
+//                     frame_index += 1;
+//                 }
+//                 Ok(())
+//             };
 
-        for (stream, packet) in ictx.packets() {
-            if stream.index() == video_stream_index {
-                decoder.send_packet(&packet)?;
-                receive_and_process_decoded_frames(&mut decoder)?;
-            }
-        }
-        decoder.send_eof()?;
-        receive_and_process_decoded_frames(&mut decoder)?;
-    }
-    Ok(())
-}
+//         for (stream, packet) in ictx.packets() {
+//             if stream.index() == video_stream_index {
+//                 decoder.send_packet(&packet)?;
+//                 receive_and_process_decoded_frames(&mut decoder)?;
+//             }
+//         }
+//         decoder.send_eof()?;
+//         receive_and_process_decoded_frames(&mut decoder)?;
+//     }
+//     Ok(())
+// }
