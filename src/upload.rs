0ee184d216856329560ab8cf9e988977f5f55bf4
Nessaj <ksnessaj@hotmail.com>
2024-08-06 15:26:07 +0800

    dbg

--- src/upload.rs
+++ src/upload.rs
@@ -166,6 +166,7 @@ impl UploaderWorker {
                     info!("token retrieved: {token}");
                 }
                 while let Some(res) = rx_odres.recv().await {
+                    info!("Uploading detection: {res:?}");
                     if let None = uploader.token.as_ref() {
                         if let Ok(token) = uploader.get_token().await {
                             info!("token retrieved: {token}");
@@ -183,7 +184,6 @@ impl UploaderWorker {
     pub fn upload_odres(&self, od_res: OdResults) -> Result<(), UpError> {
         // Upload results only if including `person` or 5 more `hat`.
         if od_res.iter().any(|res| res.0 == "person") || od_res.len() > 5 {
-            info!("Uploading detection: {od_res:?}");
             match self.tx_odres.blocking_send(od_res) {
                 Ok(_) => Ok(()),
                 Err(e) => Err(UpError::ChannelError(e.to_string())),
